cf4a488ca63d52f325abe6d90d3c377dedcc0170
Julia Longtin <julia.longtin@gmail.com>
2024-11-18 18:01:56 +0000

    implement examples 2.1, 2.2, 2.3, and 2.4.

--- programs/trainLLM.hs
+++ programs/trainLLM.hs
@@ -1,7 +1,7 @@
 {- ORMOLU_DISABLE -}
 -- LLM Trainer, following the book "Build a Large Language Model (From Scratch)".
 {-
- - Copyright 2019 Julia Longtin
+ - Copyright 2024 Julia Longtin
  -
  - This program is free software: you can redistribute it and/or modify
  - it under the terms of the GNU Affero General Public License as published by
@@ -17,21 +17,215 @@
  - along with this program.  If not, see <http://www.gnu.org/licenses/>.
  -}
 
-import Prelude (Integer, IO, Maybe(Nothing), (<$>), (<*>), (>>=), (<>), pure, putStrLn)
+{-# LANGUAGE FlexibleInstances #-}
 
-import Options.Applicative (Parser, execParser, fullDesc, header, helper, info, progDesc)
+import Prelude (Bool(True, False), Char, Int, IO, Maybe(Just, Nothing), String, (<$>), (<*>), (>>=), (<>), (&&), ($), (*), (+), (-), concat, error, getContents, length, lookup, not, otherwise, putStrLn, readFile, return, show, take, zip)
+
+import Data.Char (digitToInt, isDigit, isHexDigit)
+
+import Data.List ((++), drop, elem, foldr1, head)
+
+import Data.List.Split (dropBlanks, oneOf, onSublist, split, splitOneOf)
+
+import Data.List.Unique (sortUniq)
+
+import Data.Tuple (swap)
+
+import Options.Applicative (Parser, ReadM, execParser, fullDesc, header, help, helper, info, long, metavar, option, optional, progDesc, short, str, strOption, switch)
 
 data TrainRootOpts =
   TrainRootOpts
     {
-      example :: Maybe (Integer, Integer)
+      inputFileOpt :: Maybe String
+    , exampleOpt :: Example
+    , verboseFlag :: Maybe Bool
     }
 
-trainOpts :: Parser TrainRootOpts
-trainOpts = TrainRootOpts <$> pure Nothing
+data Example =
+  Example (Int, Int)
 
+-- | Read the example number. always in the format "X.YY" where X is a single digit hexidecimal value, and YY is a 1-2 digit integer.
+exampleReader :: ReadM Example
+exampleReader = do
+  v <- str
+  let
+    exampleDescription = "An example must consist of one character for the chapter (0-9A-F), and two characters for the listing number within the chapter (00-99)."
+    findExample :: [Char] -> (Int, Int)
+    findExample [] = error $ "empty chapter.\n" <> exampleDescription
+    findExample [_] = error $ "no listing number.\n" <> exampleDescription
+    findExample (_:'.':[]) = error $ "no listing number.\n" <> exampleDescription
+    findExample (_:_:[]) = error $ "two digit chapter number.\n" <> exampleDescription
+    findExample (a:'.':b:[])
+      | isHexDigit a && isDigit b = (digitToInt a, digitToInt b)
+      | isHexDigit a = error $ "unable to read listing number.\n" <> exampleDescription
+      | isDigit b = error $ "unable to read chapter number.\n" <> exampleDescription
+      | otherwise = error $ "unable to read chapter or listing number.\n" <> exampleDescription
+    findExample (a:'.':b:c:[])
+      | isHexDigit a && isDigit b && isDigit c = (digitToInt a, (digitToInt b)*10+digitToInt c)
+      | not (isDigit c) = error $ "unable to read last digit of listing number.\n" <> exampleDescription
+      | not (isDigit b) = error $ "unable to read first digit of listing number.\n" <> exampleDescription
+      | not (isHexDigit a) = error $ "unable to read chapter number.\n" <> exampleDescription
+    findExample _ = error $ "complete failure to parse chapter and listing numbers.\n" <> exampleDescription
+  return $ Example $ findExample v
+
+trainOpts :: Parser TrainRootOpts
+trainOpts =
+  TrainRootOpts
+  <$> optional (
+  strOption
+    (    short 'i'
+      <> long "inFile"
+      <> metavar "INPUTFILE"
+      <> help "load an ASCII text file"
+    )
+  )
+  <*> (
+  option exampleReader
+    (    long "example"
+      <> short 'e'
+      <> metavar "EXAMPLE"
+      <> help "which example to run"
+    )
+  )
+  <*> optional (
+  switch
+    (    long "verbose"
+      <> short 'v'
+      <> help "whether to be verbose"
+    )
+  )
+
+example_2_1 :: [Char] -> [Char]
+example_2_1 text = "Total number of character: " <> show (length text) <> "\n" <> take 99 text
+
+example_2_2 :: [Char] -> [([Char], Int)]
+example_2_2 text = take 51 $ vocabOfText text
+
+-- | For example 2.3, they use it twice, we just implement this as two functions: one for encoding, and one for decoding.
+-- This is the encoding one.
+example_2_3_1 :: [Char] -> [Char] -> [Int]
+example_2_3_1 text string = tokensFromString vocab string
+  where
+    vocab = vocabOfText text
+
+-- | For example 2.3, they use it twice, we just implement this as two functions: one for encoding, and one for decoding.
+-- This is the decoding one.
+example_2_3_2 :: [Char] -> [Int] -> [Char]
+example_2_3_2 text tokens = stringFromTokens vocab tokens
+  where
+    vocab = vocabOfText text
+
+-- | Example 2.4 has several sub examples. This one prints the last 5 tokens in our extended vocabulary.
+example_2_4_1 :: [Char] -> [([Char], Int)]
+example_2_4_1 text = drop (length vocab - 3) $ extendedVocab vocab
+  where
+    vocab = vocabOfText text
+    extendedVocab v = v ++ [("<|endoftext|>", length v), ("<|unk|>", length v + 1)]
+
+-- Example 2.4 has several sub examples. This one gives us the tokens at the top of page 32.
+example_2_4_2 :: [Char] -> [Char] -> [Int]
+example_2_4_2 text string = getTokensFromString (extendedVocab vocab) (Just $ length (extendedVocab vocab) - 1) string
+  where
+    vocab = vocabOfText text
+    extendedVocab v = v ++ [("<|endoftext|>", length v), ("<|unk|>", length v + 1)]
+
+-- Example 2.4 has several sub examples. This one gives us the reconstituted string on page 32.
+example_2_4_3 :: [Char] -> [Int] -> [Char]
+example_2_4_3 text tokens = stringFromTokens (extendedVocab vocab) tokens
+  where
+    vocab = vocabOfText text
+    extendedVocab v = v ++ [("<|endoftext|>", length v), ("<|unk|>", length v + 1)]
+
+-- A typeclass for tokenization.
+class Tokenable s where
+  -- | Establish a vocabulary from a set of strings.
+  vocabOfText :: s -> [([Char], Int)]
+  -- Use a vocabulary to reconstruct tokens into a string.
+  stringFromTokens :: [([Char], Int)] -> [Int] -> s
+  -- Use a vocabulary to tokenize an input.
+  tokensFromString :: [([Char], Int)] -> s -> [Int] 
+
+instance Tokenable [Char] where
+  vocabOfText s = vocabFromText s
+  stringFromTokens v t = getStringFromTokens v t
+  tokensFromString v s = getTokensFromString v Nothing s
+
+vocabFromText :: [Char] -> [([Char], Int)]
+vocabFromText input = zip vocab [0,1..]
+  where
+    vocab = sortUniq $ splitString input
+
+-- | split up a string into tokens. yes, the barriers between tokens are arbitrary, matching the ones used in the book.
+splitString :: [Char] -> [[Char]]
+splitString input = concat $ split (dropBlanks $ onSublist "--") <$> separatePunctuation 
+  where
+    separatePunctuation = concat $ split (oneOf ",.:;?_!\"()'") <$> words
+    words = splitOneOf " \n" input
+
+-- | Use a list of tokens to tokenize a string. optionally accepts an unknown token.
+getTokensFromString :: [([Char], Int)] -> Maybe Int -> [Char] -> [Int]
+getTokensFromString vocab unk string = findTokenOfString <$> splitString string
+  where
+    findTokenOfString :: [Char] -> Int
+    findTokenOfString s = case lookup s vocab of
+                               Just t -> t
+                               Nothing -> case unk of
+                                            Nothing -> error $ "cannot find a token for \"" <> s <> "\"\n"
+                                            Just t -> t
+
+getStringFromTokens :: [([Char], Int)] -> [Int] -> [Char]
+getStringFromTokens vocab tokens = maybeIntersperse " " $ findStringOfToken <$> tokens
+  where
+    maybeIntersperse :: [Char] -> [[Char]] -> [Char]
+    maybeIntersperse _ [] = []
+    maybeIntersperse x xs = foldr1 maybeIntersperse' xs
+      where
+        maybeIntersperse' :: [Char] -> [Char] -> [Char]
+        maybeIntersperse' a b = case (head b) `elem` ",.?!\"()'" of
+                                  False -> a ++ x ++ b
+                                  True -> a ++ b
+    findStringOfToken t = case lookup t bacov of
+                            Just s -> s
+                            Nothing -> error $ "cannot find a string for token" <> (show t) <> "\n"
+    bacov = swap <$> vocab
+
+-- | select which example to run.
 run :: TrainRootOpts -> IO ()
-run rawArgs = putStrLn "Hello World"
+run rawArgs =
+  let
+    readInput :: IO String
+    readInput = do
+      input <- case inputFileOpt rawArgs of
+                 Nothing -> getContents
+                 Just inFile -> readFile inFile
+      return input
+    beVerbose = case verboseFlag rawArgs of
+                  Nothing -> False
+                  Just a -> a
+  in
+    case exampleOpt rawArgs of
+      Example (2,1) -> do
+        input <- readInput
+        case beVerbose of
+          True -> putStrLn $ "running listing 2.1 with input:\n<input>\n" <> input <> "<\\input>\n<result>" <> example_2_1 input <> "<\result>\n"
+          _ -> putStrLn $ example_2_1 input
+      Example (2,2) -> do
+        input <- readInput
+        putStrLn $ show $ example_2_2 input
+      Example (2,3) -> do
+        input <- readInput
+        putStrLn $ (show $ example_2_3_1 input example_2_3_String) <> "\n"
+                 <> (show $ example_2_3_2 input $ example_2_3_1 input example_2_3_String) <> "\n"
+      Example (2,4) -> do
+        input <- readInput
+        putStrLn $ (show $ example_2_4_1 input) <> "\n"
+                 <> (show example_2_4_String) <> "\n"
+                 <> (show $ example_2_4_2 input example_2_4_String) <> "\n"
+                 <> (show $ example_2_4_3 input $ example_2_4_2 input example_2_4_String) <> "\n"
+      Example (a,b) -> error $ "unknown listing: " <> show a <> "." <> show b <> "\n"
+  where
+    example_2_3_String = "\"It's the last he painted, you know,\" Mrs. Gisburn said with pardonable pride."
+    example_2_4_String = "Hello, do you like tea?" <> " <|endoftext|> " <> "In the sunlit terraces of the palace."
 
 -- | The entry point. Use the option parser then run the trainer.
 main :: IO ()
